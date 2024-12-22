cb1065b614cb8b0763c0fbca8a4b0357774e4a73
noviceGuru <amin.arabi@gmail.com>
2024-12-22 00:34:26 -0500

    text input added to mobile, sends a message with id, date and text

--- tabarnak-mobile/app/index.tsx
+++ tabarnak-mobile/app/index.tsx
@@ -1,20 +1,40 @@
-import { useEffect } from "react"
-import { Button, Text, View } from "react-native"
+import { useEffect, useState, useRef } from "react"
+import { Button, Dimensions, TextInput, View } from "react-native"
 
 const URL = "ws://localhost:8000"
 
 export default function Index() {
-    let ws: WebSocket
-    useEffect(() => {
-        ws = new WebSocket(URL)
+    const [inputText, setInputText] = useState<string>("")
+    const wsRef = useRef<WebSocket | null>(null)
 
-        ws.onopen = e => {
+    useEffect(() => {
+        wsRef.current = new WebSocket(URL)
+        wsRef.current.onopen = e => {
             console.log(e, "OPENED")
+        }
+
+        // Clean up WebSocket connection on unmount
+        return () => {
+            if (wsRef.current) {
+                wsRef.current.close()
+            }
         }
     }, [])
 
     const sendMessage = () => {
-        ws.send(JSON.stringify({ name: "TABARANAAAAK" }))
+        if (wsRef.current?.readyState === WebSocket.OPEN) {
+            wsRef.current.send(
+                JSON.stringify({
+                    message: {
+                        id: Date.now(),
+                        date: new Date().toISOString(),
+                        message: inputText,
+                    },
+                })
+            )
+        } else {
+            console.log("WebSocket is not connected")
+        }
     }
 
     return (
@@ -23,9 +43,21 @@ export default function Index() {
                 flex: 1,
                 justifyContent: "center",
                 alignItems: "center",
+                gap: 200,
             }}
         >
-            <Button title="Tabarnak!" onPress={sendMessage}></Button>
+            <TextInput
+                style={{
+                    height: 50,
+                    backgroundColor: "lightblue",
+                    marginHorizontal: 100,
+                    paddingHorizontal: 10,
+                    borderRadius: 10,
+                    width: Dimensions.get("window").width - 200,
+                }}
+                onChangeText={text => setInputText(text)}
+            />
+            <Button title="Tabarnak!" onPress={sendMessage} />
         </View>
     )
 }
